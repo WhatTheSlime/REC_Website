c6350eaece7c87a4bb62325b7a06fb35c742ebec
WhatTheSlime <selim.lanouar@gmail.com>
2018-03-27 12:39:22 +0200

    DEBUG == False

--- rec/urls.py
+++ rec/urls.py
@@ -17,6 +17,7 @@ from django.conf.urls import url, include
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.conf import settings
+from django.views.generic import RedirectView
 
 urlpatterns = [
     url(r'^.*/$', RedirectView.as_view(url='http://radioesieaclub.pythonanywhere.com/blog/accueil', permanent=False)),
